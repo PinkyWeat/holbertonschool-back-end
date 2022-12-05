65eb5e89d9e563095c584aaa8193fcabbda274ec
Denisse Landau <pinkyweat15@@gmail.com>
2022-12-04 23:50:05 -0300

    dd

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-fffffffWrite a Python script that, using this REST API, for a given employee ID,
+frite a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
